c16bdf776900b65cbeb35f8d4f19ba903881d896
camilo.calvoalcaniz <camilo.calvoalcaniz@zoll.com>
2024-02-23 10:36:22 -0500

    Instead of importing an image from the /public directory, allowing the user to upload it with @capacitor/camera. After that, began seeing the memory leak issue.

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -1,5 +1,19 @@
-import React from "react";
-import { IonButton, IonContent, IonImg, IonPage } from "@ionic/react";
+import React, { useEffect, useState } from "react";
+import {
+  IonButton,
+  IonContent,
+  IonImg,
+  IonItem,
+  IonList,
+  IonPage,
+} from "@ionic/react";
+import {
+  CameraResultType,
+  Camera,
+  CameraSource,
+  CameraDirection,
+  Photo,
+} from "@capacitor/camera";
 
 interface DashboardProps {
   setIsLoggedIn: React.Dispatch<React.SetStateAction<boolean>>;
@@ -8,8 +22,46 @@ interface DashboardProps {
 const Dashboard = (props: DashboardProps) => {
   const { setIsLoggedIn } = props;
 
+  const [photo, setPhoto] = useState<Photo | undefined>(undefined);
+
+  const takePhoto = async () => {
+    await Camera.getPhoto({
+      quality: 60,
+      allowEditing: false,
+      resultType: CameraResultType.Base64,
+      saveToGallery: false,
+      source: CameraSource.Camera,
+      presentationStyle: "fullscreen",
+      correctOrientation: true,
+      width: 2048,
+      height: 1536,
+      direction: CameraDirection.Rear,
+    })
+      .then((photo) => {
+        setPhoto(photo);
+      })
+      .catch((error) => {
+        console.error(error.message);
+      });
+  };
+
+  useEffect(() => {
+    takePhoto();
+  }, []);
+
   const handleLogin = () => {
     setIsLoggedIn(false);
+  };
+
+  const renderCapturedImage = () => {
+    if (!photo) return <></>;
+
+    return (
+      <IonImg
+        src={`data:image/${photo.format};base64,${photo.base64String}`}
+        alt=""
+      />
+    );
   };
 
   return (
@@ -17,8 +69,7 @@ const Dashboard = (props: DashboardProps) => {
       <IonContent>
         Dashboard (Logged In)
         <IonButton onClick={() => handleLogin()}>Log Out</IonButton>
-        <div style={{ height: "200px", width: "1px" }} />
-        <IonImg src={"/hawaii.jpeg"} />
+        {renderCapturedImage()}
       </IonContent>
     </IonPage>
   );
